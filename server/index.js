9ad926becba4db903a42918c071c1f2a81a72fa6
Prashant Jha <prjha@C02X60DSJG5J.ad.here.com>
2021-10-10 18:48:57 +0530

    reusing exiting components and putting final code

--- server/index.js
+++ server/index.js
@@ -20,17 +20,28 @@ io.on('connection', (socket) => {
     console.log("We have a new connection!");
     socket.on('join', ({name, room}, callback) => {
         console.log(`Hello from ${name} who just joined room ${room}!`)
-        const {error, user} = addUser({id:socket.id, name,room});
+        const {error, user} = addUser({id:socket.id,name,room});
         if(error)
             return callback(error);
         socket.emit('meesage', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined.`})
         socket.join(user.room)
     })
+
+    socket.on('sendMessage',(message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message',{
+            user: user.name,
+            text: message
+        })
+    })
+
     socket.on('disconnect', () => {
         console.log("User has left!")
     })
 });
+
+
 
 app.use(require('cors')());
 app.use(router)
